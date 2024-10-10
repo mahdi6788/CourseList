55ffdc46b6764d007472a9a3abcf44a506a1ae99
mahdi <mjalalimech@gmail.com>
2024-10-10 18:21:22 +0400

    function delete and complete

--- src/App.jsx
+++ src/App.jsx
@@ -4,28 +4,42 @@ import AddNewNote from "./components/AddNewNote";
 import Header from "./components/Header";
 import NotesList from "./components/NotesList";
 
-
 function App() {
-  // 
-  const [notes, setNotes] = useState([])
+  //
+  const [notes, setNotes] = useState([]);
   const handleAddNotes = (newNote) => {
-    setNotes(prevNotes => [...prevNotes, newNote])
-  }
+    setNotes((prevNotes) => [...prevNotes, newNote]);
+  };
+  const handleDelete = (id) => {
+    const filtered = notes.filter((note) => note.id !== id);
+    setNotes(filtered);
+  };
+  const handleCheckBox = (id) => {
+    setNotes((prev) =>
+      prev.map((note) =>
+        note.id === id ? { ...note, completed: !note.completed } : note
+      )
+    );
+  };
 
   return (
     <div className="component">
-      <div className="note-header"> <Header /> </div>
+      <div className="note-header">
+        <Header />
+      </div>
       <div className="note-app">
         {/* These two components are sibling */}
-        <AddNewNote onAddNote={handleAddNotes}/>
+        <AddNewNote onAddNote={handleAddNotes} />
         <div className="note-container">
-          <NotesList notes={notes}/>
+          <NotesList
+            notes={notes}
+            handleDelete={handleDelete}
+            handleCheckBox={handleCheckBox}
+          />
         </div>
       </div>
-
-
     </div>
-  )
+  );
 }
 
 export default App;
--- src/components/NotesList.jsx
+++ src/components/NotesList.jsx
@@ -1,36 +1,43 @@
-function NotesList({notes}) {
-    return (
-        <div>
-            {notes.map(note => <NoteItem note={note} key={note.id} />)}
-        </div>
-    )    
+function NotesList({ notes, handleDelete, handleCheckBox }) {
+  return (
+    <div>
+      {notes.map((note) => (
+        <NoteItem
+          note={note}
+          key={note.id}
+          handleDelete={handleDelete}
+          handleCheckBox={handleCheckBox}
+        />
+      ))}
+    </div>
+  );
 }
 
-export default NotesList
+export default NotesList;
 
-function NoteItem({note}){
-    // format to display date
-    const option = {
-        year:"numeric",
-        month:"long",
-        day:"numeric"
-    }
+function NoteItem({ note, handleDelete, handleCheckBox }) {
+  // format to display date
+  const option = {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  };
 
-    return (
-    <div className="note-item">
-        <div className="note-item__header">
-            <div>
-                <p className="title">{note.title}</p>
-                <p className="desc">{note.description}</p>
-            </div>
-            <div className="actions">
-                <button>X</button>
-                <input type="checkbox" />
-            </div>
+  return (
+    <div className={`note-item ${note.completed && "completed" }`}>
+      <div className="note-item__header">
+        <div>
+          <p className="title">{note.title}</p>
+          <p className="desc">{note.description}</p>
         </div>
-        <div className="note-item__footer">
-            {new Date(note.createdAt).toLocaleDateString("en-US", option)}
+        <div className="actions">
+          <button onClick={() => handleDelete(note.id)}>X</button>
+          <input onChange={() => handleCheckBox(note.id)} type="checkbox" />
         </div>
+      </div>
+      <div className="note-item__footer">
+        {new Date(note.createdAt).toLocaleDateString("en-US", option)}
+      </div>
     </div>
-    )
+  );
 }
