7b58c719ff0d2528445e82de454fc675950438c7
mahdi <mjalalimech@gmail.com>
2024-10-10 11:19:37 +0400

    use onchange and on submit and states

--- src/components/AddNewNote.jsx
+++ src/components/AddNewNote.jsx
@@ -1,19 +1,53 @@
-function AddNewNote(){
-    const handleSubmit = (event) => {
-        event.preventDefault()
-    }
+import { useState } from "react";
 
+function AddNewNote() {
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
 
-    return (
-        <div className="add-new-note">
-            <h2>Add New Note</h2>
-            <form className="note-form" onSubmit={handleSubmit}>
-                <input type="text" className="text-field" placeholder="Note Title"/>
-                <input type="text" className="text-field" placeholder="Note Description"/>
-                <button type="submit" className="btn btn--primary">Add New Note</button>
-            </form>
-        </div>
-    )
+  //   using onSubmit is better than onClick on button, because we can push enter button and click on button as well.
+  const handleSubmit = (event) => {
+    // to prevent from refreshing the page
+    event.preventDefault();
+    // to store note in an object and for future usage
+    const newNote = {
+      title,
+      description,
+      id: Date.now(),
+      completed: false,
+      createdAt: new Date().toISOString(),
+    };
+    console.log(newNote);
+    // to make the input empty and ready for another note
+    setTitle("");
+    setDescription("");
+  };
+
+  //   next step: updating the existing notes
+
+  return (
+    <div className="add-new-note">
+      <h2>Add New Note</h2>
+      <form className="note-form" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="text-field"
+          placeholder="Note Title"
+          value={title}
+          onChange={(event) => setTitle(event.target.value)}
+        />
+        <input
+          type="text"
+          className="text-field"
+          placeholder="Note Description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+        />
+        <button type="submit" className="btn btn--primary">
+          Add New Note
+        </button>
+      </form>
+    </div>
+  );
 }
 
-export default AddNewNote
+export default AddNewNote;
