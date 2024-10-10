ffa85240591a600fc6adba72cd894c3fd7340ce1
mahdi <mjalalimech@gmail.com>
2024-10-10 10:35:38 +0400

    make an initial component for header
    import and put it into app component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import Header from "./components/Header";
 
 
 function App() {
   return (
     <div className="component">
-      <div className="note-header">Header</div>
+      <div className="note-header"> <Header /> </div>
       <div className="note-app">
         <div className="add-new-note">AddNote</div>
         <div className="note-container">Notes</div>
