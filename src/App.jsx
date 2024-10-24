1f975b36674e2feede23c1ea731dae0fde85fc0b
mahdi <mjalalimech@gmail.com>
2024-10-24 11:01:22 +0400

    useReducer instead of useState
    when we have a state that works in several roles, it is better to use useReducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,54 @@
-import { useState } from "react";
 import "./App.css";
+import { useReducer, useState } from "react";
 import AddNewNote from "./components/AddNewNote";
 import Header from "./components/Header";
 import NotesList from "./components/NotesList";
 import NoteStatus from "./components/NoteStatus";
 
+function noteReducer(notes, {type, payload}) {
+  switch (type) {
+    case "add": {
+      return [...notes, payload];
+    }
+    case "delete": {
+      return notes.filter((note) => note.id !== payload);
+    }
+    case "complete": {
+      return notes.map((note) =>
+        note.id === payload ? { ...note, completed: !note.completed } : note
+      );
+    }
+    default: throw new Error ("Unknown error" + type)
+  }
+}
+
 function App() {
-  //
-  const [notes, setNotes] = useState([]);
+  // const [notes, setNotes] = useState([]);
+  const [notes, dispatch] = useReducer(noteReducer, []);
+
   const handleAddNotes = (newNote) => {
-    setNotes((prevNotes) => [...prevNotes, newNote]);
+    // setNotes((prevNotes) => [...prevNotes, newNote]);
+    dispatch({ type: "add", payload: newNote });
   };
   const handleDelete = (id) => {
-    const filtered = notes.filter((note) => note.id !== id);
-    setNotes(filtered);
+    // const filtered = notes.filter((note) => note.id !== id);
+    // setNotes(filtered);
+    dispatch({ type: "delete", payload: id });
   };
   const handleCheckBox = (id) => {
-    setNotes((prev) =>
-      prev.map((note) =>
-        note.id === id ? { ...note, completed: !note.completed } : note
-      )
-    );
+    // setNotes((prev) =>
+    //   prev.map((note) =>
+    //     note.id === id ? { ...note, completed: !note.completed } : note
+    //   )
+    // );
+    dispatch({ type: "complete", payload: id });
   };
   // to sort notes
   const [sortBy, setSortBy] = useState("");
 
   return (
     <div className="component">
-      <Header notes={notes} setSortBy={setSortBy} sortBy={sortBy}/>
+      <Header notes={notes} setSortBy={setSortBy} sortBy={sortBy} />
       <div className="note-app">
         {/* These two components are sibling */}
         <AddNewNote onAddNote={handleAddNotes} />
